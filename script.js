6c818df6d8ee250fab1e90baff8417cb2bd4e5e0
Alihan Dok <156813859+kirikisanso@users.noreply.github.com>
2024-07-03 20:30:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// let regex = 
+
+// let str = "liver bro live!"
+// let res = /liver/i
+// console.log(res.test(str))
+
+// alert('We will, i will'.replace(/we/ig,'I'))
+
+// function validitionEmail(event){
+//      let value = event.target.value;
+//     const re =
+//       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+//     re.test(value)
+//       ? p.innerHTML = "<span class='tr'>правилно, молодец</span>"
+//       : p.innerHTML = "<span class='fl'>неправильно, подумай ещё</span>";
+//     }  
+
+//     validitionEmail('sloganloh69@gmail.com')
